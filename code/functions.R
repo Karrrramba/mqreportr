cf2ab14cbf509a245b65d36cc725575a6d8cdcba
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-08-07 01:29:01 +0200

    Fixed basic file loading and cleaning. Added annotation file creation

--- code/functions.R
+++ code/functions.R
@@ -19,48 +19,145 @@ for (t in mq_tables) {
   }
 }
 
-load_table <- function(df, multi) {
-  
-  cols <- c("Protein IDs",
-            "Reverse",
-            "Potential contaminant")
-  if (stringr::str_detect(df, "site")) {
-    cols <- append(cols, c())
-  }
-  
-  if (multi == "tmt") {
-    quant_columns <- paste0("matches(", "^(LFQ) |(I|i)ntensity .*", ")")
-    cols <- append(cols, quant_columns)
-  } else if (multi == "lfq") {
-    quant_columns <- paste0("matches(", "Reporter intensity (corrected|\\d)", ")")
-    cols <- append(cols, quant_columns)
-  } else {
-    quant_columns <- paste0("matches(", "Ratio .*", ")")
-    cols <- append(cols, quant_columns)
-  }
-  
-  cols <- cols[!grepl("count", cols)]
-  
-  df <- readr::read_tsv(file = df,
-                        col_select = cols)
-  return(df)
+remove_prefix <- function(data){
+  data <- data[, !(grepl("^(reporter|lfq|ratio)", colnames(data)) & !grepl("(corrected|normalized)", colnames(data)))]
+  names(data) <- trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", names(data)), which = "left", whitespace = "_")
+  return(data)
 }
 
+load_table <- function(data, quant) {
+  
+  cols <- c("Protein IDs",
+            "Protein names", 
+            "Gene names", 
+            "Reverse",
+            "Potential contaminant",
+            "Only identified by site")
+  
+  site_cols <- c("Amino acid", 
+                 "Site",
+                 "Localization prob",
+                 "Multiplicity",
+                 "Sequence window")
+  
+  # Add modification-relevant columns if applicable 
+  if (stringr::str_detect(data, "site")) {
+    cols <- c(cols, site_cols)
+  }
+  
+  # Select quantification columns based on quantification strategy
+  if (quant == "lfq") {
+    quant_regex <- "^(LFQ) |(I|i)ntensity .*"
+  } else if (quant == "tmt") {
+    quant_regex <- "Reporter intensity (corrected|\\d)"
+  } else {
+    quant_regex <- "Ratio .*"
+  }
+  
+  d <- readr::read_tsv(file = data,
+                        col_select = c(
+                          cols,
+                          matches(quant_regex))
+                        ) %>% 
+    janitor::clean_names(., abbreviation = "ID")
+  
+  # Remove non-normalized intensity/ratio columns if applicable
+  d_clean <- d[, !(grepl("^(reporter|lfq|ratio)", colnames(d)) & !grepl("(corrected|normalized)", colnames(d)))]
+  
+  # Remove ratio counts
+
+  return(d_clean)
+}
+
+# Load all detected files into global env
 list2env(
   lapply(setNames(site_tables, janitor::make_clean_names(site_tables)), load_table, multi = "lfq"),
   envir = .GlobalEnv)
 
-### Universal data cleaning
-# Formats column names, removes reverse and contaminants 
-clean_df <- function(data) {
-  del_row <- which(data[, "Reverse"] == "+" | data[, "Potential contaminant"] == "+")
-  del_col <- which(names(data) %in% c("Reverse", "Potential contaminant"))
+### Universal data cleaning----
+
+# Removes reverse and contaminants, filters sites to desired threshold
+clean_df <- function(data, prob_treshold = 0.9) {
+  del_row <- which(data[, "reverse"] == "+" | data[, "potential_contaminant"] == "+" | data[, "only_identified_by_site"] == "+")
+  del_col <- which(names(data) %in% c("reverse", "potential_contaminant", "only_identified_by_site"))
   
   data <- data[-del_row, -del_col]
-  data <- janitor::clean_names(data, abbreviation = "ID")
+  data <- data["localization_prob" >= prob_treshold, ]
+  
   return(data)
 }
-# proteinGroups
+
+remove_prefix <- function(data){
+  names(data) <- trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", names(data)), which = "left", whitespace = "_")
+  
+  return(data)
+}
+
+write_annotation_file <- function(data) {
+  
+  nq_cols <- c("protein_id_s",
+            "protein_names", 
+            "gene_names", 
+            "amino_acid", 
+            "site",
+            "localization_prob",
+            "multiplicity",
+            "sequence_window")
+  
+  a <- data.frame(sample = names(data[, !names(data) %in% nq_cols]),
+                  keep = TRUE,
+                  group_1 = NA)
+  
+  write.csv(a, file = "annotation_file.csv")
+}
+
+
+
+
+# Read annotation file and create groups based on column names
+read_annotation_file <- function(a_file, data) {
+  
+  annotations <- read_csv(a_file)
+  
+  # Remove channels if applicable
+  annotations <- annotations["keep" == TRUE, ]
+  names(data) <- annotations[, names(data) %in% annotations$sample] 
+  
+  # Merge annotations with data to create groups
+  
+  return(data)
+}
+
+
+
+rat_pg %>% 
+  pivot_longer(
+    # cols = starts_with("reporter"), 
+    cols = matches("^(reporter|lfq|ratio|intensity)"), 
+    names_to = "sample",
+    values_to = "log2_intensity"
+    ) %>% 
+  mutate(
+    intensity = round(log2(log2_intensity),2),
+    sample = trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", sample), which = "left", whitespace = "_"),
+    tmt_channel = stringr::str_extract(sample, "^\\d+"),
+    sample = gsub("^(\\d+_)", "", sample),
+    protein_id_s = gsub("(;.+)", "", protein_id_s),
+    gene_names = gsub("(;.+)", "", gene_names),
+    )
+
+# Experimental Design Templates----
+
+
+
+
+
+# proteinGroups----
+rat_pg <- load_table('data-raw/tmt_rat/proteinGroups.txt', quant = "tmt")
+rat_pg <- clean_df(rat_pg)
+rat_pg <- remove_prefix(rat_pg)
+write_annotation_file(rat_pg)
+
 protein_groups <- readr::read_tsv("data-raw/proteinGroups.txt",
                                   col_select =  c("Protein IDs",
                                                   "Gene names",
