aeaf0a89d3552fad9908e2a30a4b67021cf534ee
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-08-07 17:17:11 +0200

    Data transformation into long format

--- code/functions.R
+++ code/functions.R
@@ -20,7 +20,6 @@ for (t in mq_tables) {
 }
 
 remove_prefix <- function(data){
-  data <- data[, !(grepl("^(reporter|lfq|ratio)", colnames(data)) & !grepl("(corrected|normalized)", colnames(data)))]
   names(data) <- trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", names(data)), which = "left", whitespace = "_")
   return(data)
 }
@@ -63,8 +62,6 @@ load_table <- function(data, quant) {
   
   # Remove non-normalized intensity/ratio columns if applicable
   d_clean <- d[, !(grepl("^(reporter|lfq|ratio)", colnames(d)) & !grepl("(corrected|normalized)", colnames(d)))]
-  
-  # Remove ratio counts
 
   return(d_clean)
 }
@@ -77,22 +74,21 @@ list2env(
 ### Universal data cleaning----
 
 # Removes reverse and contaminants, filters sites to desired threshold
-clean_df <- function(data, prob_treshold = 0.9) {
+clean_df <- function(data, prob_treshold = 0.9, remove_empty = TRUE) {
   del_row <- which(data[, "reverse"] == "+" | data[, "potential_contaminant"] == "+" | data[, "only_identified_by_site"] == "+")
   del_col <- which(names(data) %in% c("reverse", "potential_contaminant", "only_identified_by_site"))
   
   data <- data[-del_row, -del_col]
   data <- data["localization_prob" >= prob_treshold, ]
   
-  return(data)
-}
-
-remove_prefix <- function(data){
-  names(data) <- trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", names(data)), which = "left", whitespace = "_")
+  if (remove_empty) {
+    data <- remove_empty_channels(data)
+  }
   
   return(data)
 }
 
+# Creates an annotation file in the working directory
 write_annotation_file <- function(data) {
   
   nq_cols <- c("protein_id_s",
@@ -105,46 +101,31 @@ write_annotation_file <- function(data) {
             "sequence_window")
   
   a <- data.frame(sample = names(data[, !names(data) %in% nq_cols]),
-                  keep = TRUE,
                   group_1 = NA)
   
   write.csv(a, file = "annotation_file.csv")
 }
 
 
-
-
 # Read annotation file and create groups based on column names
 read_annotation_file <- function(a_file, data) {
   
-  annotations <- read_csv(a_file)
+  annotations <- readr::read_csv(a_file)
   
   # Remove channels if applicable
   annotations <- annotations["keep" == TRUE, ]
   names(data) <- annotations[, names(data) %in% annotations$sample] 
   
   # Merge annotations with data to create groups
+  data <- data %>% 
+    dplyr::left_join()
   
   return(data)
 }
 
 
 
-rat_pg %>% 
-  pivot_longer(
-    # cols = starts_with("reporter"), 
-    cols = matches("^(reporter|lfq|ratio|intensity)"), 
-    names_to = "sample",
-    values_to = "log2_intensity"
-    ) %>% 
-  mutate(
-    intensity = round(log2(log2_intensity),2),
-    sample = trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", sample), which = "left", whitespace = "_"),
-    tmt_channel = stringr::str_extract(sample, "^\\d+"),
-    sample = gsub("^(\\d+_)", "", sample),
-    protein_id_s = gsub("(;.+)", "", protein_id_s),
-    gene_names = gsub("(;.+)", "", gene_names),
-    )
+
 
 # Experimental Design Templates----
 
@@ -155,7 +136,42 @@ rat_pg %>%
 # proteinGroups----
 rat_pg <- load_table('data-raw/tmt_rat/proteinGroups.txt', quant = "tmt")
 rat_pg <- clean_df(rat_pg)
-rat_pg <- remove_prefix(rat_pg)
+
+rat_pg_nc <- rat_pg %>% 
+  dplyr::select(!(matches("^\\d") & !matches("nc")))
+
+r_pg_long <- rat_pg %>% 
+  dplyr::pivot_longer(
+    cols = matches("^(reporter|lfq|ratio|intensity)"), 
+    names_to = "sample",
+    values_to = "log2_intensity"
+  ) %>% 
+  dplyr::mutate(
+    # Remove prefix
+    sample = trimws(gsub("(reporter|lfq|ratio|intensity|corrected)", "", sample), which = "left", whitespace = "_"),
+    tmt_channel = as.numeric(stringr::str_extract(sample, "^\\d+")),
+    sample = gsub("^(\\d+_)", "", sample),
+    # Collapse ID columns to first entry
+    protein_id_s = gsub("(;.+)", "", protein_id_s),
+    gene_names = gsub("(;.+)", "", gene_names),
+  ) %>% 
+  remove_empty_channels(.) %>% 
+  # Log2 transform
+  dplyr::mutate(log2_intensity = dplyr::if_else(log2_intensity != 0, log2(log2_intensity), 0))
+
+remove_empty_channels <- function(data){
+  ch <- data %>% 
+  group_by(tmt_channel) %>% 
+  summarise(values = length(unique(log2_intensity))) %>% 
+  filter(values <= 2) %>% 
+  pull(tmt_channel)
+  
+  data <- filter(data, !tmt_channel %in% ch)
+  
+  return(data)
+}
+
+
 write_annotation_file(rat_pg)
 
 protein_groups <- readr::read_tsv("data-raw/proteinGroups.txt",
